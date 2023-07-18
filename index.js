62719a652315b2bcf6b596f32ff37924d5656a95
Josue Morales <thejosuescript@gmail.com>
2023-07-17 20:53:54 -0600

    index file, exporting all functionalities

---
+++ index.js
@@ -0,0 +1,5 @@
+const BigDecimal = require("./src/BigDecimalOps");
+const bigInteger = require("./src/BigIntegerOps");
+const IsValidNumber = require("./src/IsValidNumber")
+
+module.exports={bigInteger,BigDecimal,IsValidNumber}
