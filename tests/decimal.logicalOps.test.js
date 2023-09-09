1c7fbb1de5cc76645a1a643887c6847299f1780f
Josue Morales <thejosuescript@gmail.com>
2023-09-09 17:27:08 -0600

    logical ops testing up

--- tests/decimal.logicalOps.test.js
+++ tests/decimal.logicalOps.test.js
@@ -1,23 +1,24 @@
-const bigDecimal = require('../src/bigDecimalOps.js')
-function testing(value, be) {
-    test(`should test ${value} to be ${be}`, () => {
+const bigDecimal = require('../src/BigDecimalOps.js')
+
+function testing(name,value, be) {
+    test(`${name}:should test ${value} to be ${be}`, () => {
         expect(value).toBe(be)
     })
 }
 //logical operations
-testing(new bigDecimal('199999').gt('200000'),false)
-testing(new bigDecimal('199999').lt('200000'),true)
-testing(new bigDecimal('199999').gte('200000'),false)
-testing(new bigDecimal('199999').gte('199999'),true)
-testing(new bigDecimal('199999').lte('200000'),true)
-testing(new bigDecimal('199999').lte('199999'),true)
-testing(new bigDecimal('199999').eq('199999'),true)
-testing(bigDecimal.greaterThan('123456789123456789','123456789123456789'),false)
-testing(bigDecimal.lessThan('123456789123456789','1234567891234567891'),true)
-testing(bigDecimal.greaterOrEqualThan('1234567891234567891','123456789123456789'),true)
-testing(bigDecimal.greaterOrEqualThan('123456789123456789','123456789123456789'),true)
-testing(bigDecimal.lessOrEqualThan('1234567891234567895','123456789123456789'),false)
-testing(bigDecimal.isEqualTo('123456789123456789','123456789123456789'),true)
-testing(bigDecimal.isNaNDecimal('18.99999999999999'),false)
-testing(bigDecimal.isSafeInteger(Number.MAX_SAFE_INTEGER),true)
-testing(bigDecimal.isDecimal('18.99999999999999'),true)
+testing('gt',new bigDecimal('199999').gt('200000'),false)
+testing('lt',new bigDecimal('199999').lt('200000'),true)
+testing('gte',new bigDecimal('199999').gte('200000'),false)
+testing('gte2',new bigDecimal('199999').gte('199999'),true)
+testing('lte',new bigDecimal('199999').lte('200000'),true)
+testing('lte2',new bigDecimal('199999').lte('199999'),true)
+testing('eq',new bigDecimal('199999').eq('199999'),true)
+testing('grth',bigDecimal.greaterThan('-12345678912345678','-123456789123456789'),true)
+testing('leth',bigDecimal.lessThan('123456789123456789','1234567891234567891'),true)
+testing('greqth',bigDecimal.greaterOrEqualThan('1234567891234567891','123456789123456789'),true)
+testing('greqth',bigDecimal.greaterOrEqualThan('123456789123456789','123456789123456789'),true)
+testing('leeqth',bigDecimal.lessOrEqualThan('1234567891234567895','123456789123456789'),false)
+testing('iseq',bigDecimal.isEqualTo('123456789123456789','123456789123456789'),true)
+testing('isnand',bigDecimal.isNaNDecimal('18.99999999999999'),false)
+testing('issafe',bigDecimal.isSafeInteger(Number.MAX_SAFE_INTEGER),true)
+testing('isdec',bigDecimal.isDecimal('18.99999999999999'),true)
