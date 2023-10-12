095be7a8b6ea458504e9060932c594b60b855a08
Josue Morales <thejosuescript@gmail.com>
2023-10-12 14:35:19 -0600

    building changes

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@
 var bigDecimal = require("./BigDecimalOps.js");
 var bigInteger = require("./BigIntegerOps.js");
 var IsValidNumber = require("./IsValidNumber");
+console.log(new bigInteger(-10).ReturnModule(3));
 module.exports = {
   bigInteger: bigInteger,
   bigDecimal: bigDecimal,
